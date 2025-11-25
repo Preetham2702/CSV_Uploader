53e732cd17509804043405a0b2f89dd7aa956872
Preetham Reddy Dodda <preethamreddy2702@gmail.com>
2025-11-25 13:55:40 -0600

    updated

--- app.py
+++ app.py
@@ -1091,4 +1091,5 @@ def pico_log():
     
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
